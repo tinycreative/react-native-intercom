f3e6e18c68c62559e80986e3703033094b8b68c0
Atticus White <contact@atticuswhite.com>
2015-12-08 16:00:12 -0500

    basic structure of the interom module

--- android/src/main/java/com/robinpowered/react/Intercom/IntercomModule.java
+++ android/src/main/java/com/robinpowered/react/Intercom/IntercomModule.java
@@ -1,4 +1,59 @@
 package com.robinpowered.react.Intercom;
 
-public class IntercomModule {
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class IntercomModule extends ReactContextBaseJavaModule {
+
+    private static final String MODULE_NAME = "Intercom";
+
+    public IntercomModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return MODULE_NAME;
+    }
+
+    @ReactMethod
+    public void registerIdentifiedUser() {
+
+    }
+
+    @ReactMethod
+    public void registerUnidentifiedUser() {
+
+    }
+
+    @ReactMethod
+    public void reset() {
+
+    }
+
+    @ReactMethod
+    public void updateUser() {
+
+    }
+
+    @ReactMethod
+    public void logEvent() {
+
+    }
+
+    @ReactMethod
+    public void displayMessageComposer() {
+
+    }
+
+    @ReactMethod
+    public void setVisibility() {
+
+    }
+
+    @ReactMethod
+    public void setPreviewPosition() {
+
+    }
 }
