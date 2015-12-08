c0471d68da9784a178b7056397f8d1d4c11e9dba
Atticus White <contact@atticuswhite.com>
2015-12-08 16:45:52 -0500

    recursively deconstruct ReadableMap and ReadableArray wrappers

--- android/src/main/java/com/robinpowered/react/Intercom/IntercomModule.java
+++ android/src/main/java/com/robinpowered/react/Intercom/IntercomModule.java
@@ -6,10 +6,17 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.ReadableType;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.annotation.Nullable;
 
 import io.intercom.android.sdk.Intercom;
 import io.intercom.android.sdk.identity.Registration;
@@ -67,30 +74,77 @@ public class IntercomModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void updateUser(ReadableMap options, Callback errorCallback, Callback successCallback) {
-        Log.i(TAG, "updateUser");
-        Map<String, Object> map = new HashMap<>();
-
-//        Intercom.client().updateUser();
-        successCallback.invoke(null);
+        try {
+            Map<String, Object> map = recursivelyDeconstructReadableMap(options);
+            Intercom.client().updateUser(map);
+            Log.i(TAG, "updateUser");
+            successCallback.invoke(null);
+        } catch (Exception e) {
+            Log.e(TAG, "updateUser - unable to deconstruct argument map");
+            errorCallback.invoke(e.toString());
+        }
     }
 
-    @ReactMethod
-    public void logEvent() {
+    private Map<String, Object> recursivelyDeconstructReadableMap(ReadableMap readableMap) {
+        ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+        Map<String, Object> deconstructedMap = new HashMap<>();
+        while (iterator.hasNextKey()) {
+            String key = iterator.nextKey();
+            ReadableType type = readableMap.getType(key);
+            switch (type) {
+                case Null:
+                    deconstructedMap.put(key, null);
+                    break;
+                case Boolean:
+                    deconstructedMap.put(key, readableMap.getBoolean(key));
+                    break;
+                case Number:
+                    deconstructedMap.put(key, readableMap.getDouble(key));
+                    break;
+                case String:
+                    deconstructedMap.put(key, readableMap.getString(key));
+                    break;
+                case Map:
+                    deconstructedMap.put(key, recursivelyDeconstructReadableMap(readableMap.getMap(key)));
+                    break;
+                case Array:
+                    deconstructedMap.put(key, recursivelyDeconstructReadableArray(readableMap.getArray(key)));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Could not convert object with key: " + key + ".");
+            }
 
+        }
+        return deconstructedMap;
     }
 
-    @ReactMethod
-    public void displayMessageComposer() {
-
-    }
-
-    @ReactMethod
-    public void setVisibility() {
-
-    }
-
-    @ReactMethod
-    public void setPreviewPosition() {
-
+    private List<Object> recursivelyDeconstructReadableArray(ReadableArray readableArray) {
+        List<Object> deconstructedList = new ArrayList<>(readableArray.size());
+        for (int i = 0; i < readableArray.size(); i++) {
+            ReadableType indexType = readableArray.getType(i);
+            switch(indexType) {
+                case Null:
+                    deconstructedList.add(i, null);
+                    break;
+                case Boolean:
+                    deconstructedList.add(i, readableArray.getBoolean(i));
+                    break;
+                case Number:
+                    deconstructedList.add(i, readableArray.getDouble(i));
+                    break;
+                case String:
+                    deconstructedList.add(i, readableArray.getString(i));
+                    break;
+                case Map:
+                    deconstructedList.add(i, recursivelyDeconstructReadableMap(readableArray.getMap(i)));
+                    break;
+                case Array:
+                    deconstructedList.add(i, recursivelyDeconstructReadableArray(readableArray.getArray(i)));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Could not convert object at index " + i + ".");
+            }
+        }
+        return deconstructedList;
     }
 }
