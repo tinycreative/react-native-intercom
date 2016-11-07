99061823176f5a40c8461dcffd724359ef80959d
Jake Thompson <jake@pocketlist.co>
2016-11-07 12:20:07 -0500

    Setup the constant properly, check that the application is fully loaded before emitting an event

--- android/src/main/java/com/robinpowered/react/Intercom/IntercomEventEmitter.java
+++ android/src/main/java/com/robinpowered/react/Intercom/IntercomEventEmitter.java
@@ -1,12 +1,19 @@
 package com.robinpowered.react.Intercom;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import javax.annotation.Nullable;
+
+import java.util.Map;
+import java.util.HashMap;
 
 import io.intercom.android.sdk.Intercom;
 import io.intercom.android.sdk.UnreadConversationCountListener;
@@ -15,15 +22,23 @@ public class IntercomEventEmitter extends ReactContextBaseJavaModule {
 
     private static final String MODULE_NAME = "IntercomEventEmitter";
     public static final String TAG = "Intercom Event";
-    public static final String UNREAD_CHANGE_NOTIFICATION = "INTERCOM_UNREAD_CHANGE_NOTIFICATION";
+    private static final String UNREAD_CHANGE_NOTIFICATION = "IntercomUnreadConversationCountDidChangeNotification";
 
     public IntercomEventEmitter(ReactApplicationContext reactContext) {
         super(reactContext);
+        Intercom.client().addUnreadConversationCountListener(unreadConversationCountListener);
     }
 
     @Override
     public String getName() {
         return MODULE_NAME;
+    }
+
+    @Override
+    public Map<String, Object> getConstants() {
+        final Map<String, Object> constants = new HashMap<>();
+        constants.put("UNREAD_CHANGE_NOTIFICATION", UNREAD_CHANGE_NOTIFICATION);
+        return constants;
     }
 
     private void handleUpdateUnreadCount() {
@@ -33,9 +48,11 @@ public class IntercomEventEmitter extends ReactContextBaseJavaModule {
     }
 
     private void sendEvent(String eventName, @Nullable WritableMap params) {
-        getReactApplicationContext()
-                .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-                .emit(eventName, params);
+        if(getReactApplicationContext().hasActiveCatalystInstance()) {
+            getReactApplicationContext()
+                    .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit(eventName, params);
+        }
     }
 
     private final UnreadConversationCountListener unreadConversationCountListener = new UnreadConversationCountListener() {
