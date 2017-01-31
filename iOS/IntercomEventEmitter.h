566a723252a740e1291ebbedd4bf02c154040433
Jason Brown <jbrown@miradorfin.com>
2017-01-31 14:40:50 -0800

    Add back paths, make backwards compat

--- iOS/IntercomEventEmitter.h
+++ iOS/IntercomEventEmitter.h
@@ -6,7 +6,11 @@
 //  Copyright © 2016 Jason Brown. All rights reserved.
 //
 
+#if __has_include(<React/RCTAssert.h>)
 #import <React/RCTEventEmitter.h>
+#else // back compatibility for RN version < 0.40
+#import "RCTEventEmitter.h"
+#endif
 
 @interface IntercomEventEmitter : RCTEventEmitter
 
--- iOS/IntercomWrapper.h
+++ iOS/IntercomWrapper.h
@@ -6,9 +6,14 @@
 //  Copyright (c) 2015 Asa Miller. All rights reserved.
 //
 
-
+#if __has_include(<React/RCTAssert.h>)
 #import <React/RCTBridgeModule.h>
 #import <React/RCTUtils.h>
+#else // back compatibility for RN version < 0.40
+#import "RCTBridgeModule.h"
+#import "RCTUtils.h"
+#endif
+
 
 @interface IntercomWrapper : NSObject <RCTBridgeModule>
 @end
