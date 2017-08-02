ff6cb8bb66cfadee4fee1a16ed21e8352f4c870f
Radek Czemerys <radko93@gmail.com>
2017-08-02 18:49:20 +0200

    Remove depreciated createJSModules

--- android/src/main/java/com/robinpowered/react/Intercom/IntercomPackage.java
+++ android/src/main/java/com/robinpowered/react/Intercom/IntercomPackage.java
@@ -25,11 +25,6 @@ public class IntercomPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList();
     }
