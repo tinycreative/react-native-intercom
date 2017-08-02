2a5ee0147f0e2e74ad3d788a2bd3d7dac0fb6552
Radek Czemerys <radko93@gmail.com>
2017-08-02 19:44:05 +0200

    Added comment

--- android/src/main/java/com/robinpowered/react/Intercom/IntercomPackage.java
+++ android/src/main/java/com/robinpowered/react/Intercom/IntercomPackage.java
@@ -24,6 +24,7 @@ public class IntercomPackage implements ReactPackage {
         return modules;
     }
     
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {		
         return Collections.emptyList();		
     }
