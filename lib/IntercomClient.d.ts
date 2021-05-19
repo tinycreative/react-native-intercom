6e8f0bcb3c1370b7981ef9a5cb926e17f0668355
Jason Brown <browniefed@gmail.com>
2021-05-19 08:23:56 -0700

    Fix and add ts

---
+++ lib/IntercomClient.d.ts
@@ -0,0 +1,47 @@
+/**
+ * @class IntercomClient
+ */
+export declare const Visibility: {
+    VISIBLE: string;
+    GONE: string;
+};
+declare class IntercomClient {
+    static Visibility: {
+        VISIBLE: string;
+        GONE: string;
+    };
+    Notifications: {
+        UNREAD_COUNT: any;
+        WINDOW_DID_HIDE: any;
+        WINDOW_DID_SHOW: any;
+    };
+    _eventEmitter: any;
+    _eventHandlers: Record<string, any>;
+    constructor();
+    registerIdentifiedUser(options: any): any;
+    sendTokenToIntercom(token: any): any;
+    updateUser(options: any): any;
+    registerUnidentifiedUser(): any;
+    logout(): any;
+    logEvent(eventName: any, metaData: any): any;
+    handlePushMessage(): any;
+    displayMessenger(): any;
+    hideMessenger(): any;
+    displayMessageComposer(): any;
+    displayMessageComposerWithInitialMessage(message: any): any;
+    displayConversationsList(): any;
+    getUnreadConversationCount(): any;
+    displayHelpCenter(): any;
+    setLauncherVisibility(visibility: any): any;
+    setInAppMessageVisibility(visibility: any): any;
+    setupAPN(deviceToken: any): any;
+    registerForPush(): any;
+    setUserHash(userHash: any): any;
+    setBottomPadding(padding: any): any;
+    presentCarousel(carousel: any): any;
+    presentArticle(articleId: any): any;
+    addEventListener(type: any, handler: any): void;
+    removeEventListener(type: any, handler: any): void;
+}
+declare const _default: IntercomClient;
+export default _default;
